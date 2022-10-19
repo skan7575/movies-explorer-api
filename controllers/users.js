d83dbe6b166423d08b2a7ab0e2b3145048c09e14
skan7575 <Ubiquity LOCOM5>
2022-10-19 20:20:36 +0300

    Level 1

--- controllers/users.js
+++ controllers/users.js
@@ -21,6 +21,30 @@ const getCurrentUser = (req, res, next) => {
       next(err)
     });
 }
+
+const UpdateCurrentUser = async (req, res, next) => {
+  const userId = req.user._id;
+  const {email, name} = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { name, email },
+      { new: true, runValidators: true },
+    );
+    if (!user) {
+      throw NotFoundError('Пользователь по указанному _id не найден.');
+    }
+    return res.status(200).send(user);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      next(new ValidationError('Введены не некорректные данные'));
+    } else {
+      next(err);
+    }
+  }
+  return res;
+};
+
 const createUser = (req, res, next) => {
   const {email, password, name} = req.body
   User.findOne({email})
@@ -60,6 +84,7 @@ const login = (req, res, next) => {
 };
 
 module.exports = {
+  UpdateCurrentUser,
   login,
   getCurrentUser,
   createUser
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,7 +1,8 @@
-const {getCurrentUser, createUser} = require("../controllers/users");
+const {getCurrentUser, createUser, UpdateCurrentUser} = require("../controllers/users");
 const usersRoutes = require('express').Router();
 
 usersRoutes.get('/me', getCurrentUser)
+usersRoutes.patch('/me', UpdateCurrentUser)
 usersRoutes.patch('/', createUser)
 
 module.exports = usersRoutes;
