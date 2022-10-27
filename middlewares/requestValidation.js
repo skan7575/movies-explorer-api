a06bb9a026c16b485d75c43f3d3c84226d25dbfc
skan7575 <Ubiquity LOCOM5>
2022-10-27 15:24:40 +0300

    Исправил длину пароля по умолчанию

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -26,7 +26,7 @@ const validateLogin = celebrate({
 const validateSignup = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required(),
+    password: Joi.string().required().min(8).max(30),
     name: Joi.string().required().min(2).max(30),
   }),
 });
