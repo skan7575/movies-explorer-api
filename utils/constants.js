8f5d1cfbeade4abc805b09dad1aa387c4793ef47
skan7575 <Ubiquity LOCOM5>
2022-10-27 15:28:49 +0300

    Исправил длину пароля по умолчанию

--- utils/constants.js
+++ utils/constants.js
@@ -1,14 +1,4 @@
-// const BAD_REQUEST = 'Неверно переданы данные';
-// const INTERNAL_SERVER_ERROR = 'На сервере произошла ошибка';
 const BAD_URL = 'не является URL адресом';
-// const NOT_FOUND_MOVIE_ERROR_MESSAGE = 'Нет фильма с таким id';
-// const VALIDATION_ERROR_NAME = 'ValidationError';
-// const ERROR_KIND_OBJECT_ID = 'ObjectId';
-// const FORBIDDEN_DELETE_MOVIE_MESSAGE = 'Нет доступа к удалению фильма';
-// const NOT_FOUND_ERROR_MESSAGE = 'Запрашиваемый ресурс не найден';
-// const NOT_AUTH_ERROR = 'Необходима авторизация';
-// const NOT_AUTH_ERROR_WRONG_EMAIL_PASSWORD = 'Неправильные почта или пароль';
-
 const USER_SCHEMA_REQUIRED_MESSAGES = {
   EMAIL: 'Поле-строка "email - электронная почта" является обязательным',
   PASSWORD: 'Поле-строка "password - пароль" является обязательным',
